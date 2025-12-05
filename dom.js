a7df08acf5f1917ba20dee11d2dcb63dbf917e2b
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2025-12-05 20:53:21 +0530

    Add button click event to update paragraph text

---
+++ dom.js
@@ -0,0 +1,7 @@
+let btn=document.getElementById("btn")
+let para=document.getElementById("para")
+
+btn.addEventListener("click",()=>{
+   para.innerText="You Clicked The Button ! Using JS"
+    
+});
